fb35b762f0f8fbadda1e623b4b8af050099121c3
raul <raul.alexander.floress@gmail.com>
2022-09-16 16:14:22 -0500

    suggested fix

--- components/SuggestedAccounts.tsx
+++ components/SuggestedAccounts.tsx
@@ -20,7 +20,7 @@ const SuggestedAccounts = () => {
       <p className='text-gray-500 font-semibold m-3 mt-4 hidden xl:block'>Suggested Accounts</p>
 
       <div>
-        {allUsers.slice(0,6).map((user: IUser )=>(
+        {allUsers.map((user: IUser )=>(
           <Link href={`/profile/${user._id}`} key={user._id} >
             <div className='p-2 cursor-pointer font-semibold rounded hover:bg-primary flex gap-3'>
               <div className='w-8 h-8'>
